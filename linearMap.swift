b99443e9a0754fccd873ccfdb78fcce8581cbe22
gideonrab <44717463+gideonrab@users.noreply.github.com>
2018-11-02 23:32:41 -0400

    cleaned up maps and a map protocol
    note: the maps are a bit more barebones.  for example, the hashmap does not track the number of collisions.  If you think that is necessary, I can add that.

---
+++ linearMap.swift
@@ -0,0 +1,52 @@
+//
+//  linearMap.swift
+//  Maps Benchmark
+//
+//  Created by Gideon Rabson on 11/2/18.
+//  Copyright © 2018 Gideon Rabson. All rights reserved.
+//
+
+import Foundation
+
+struct LinearMap <Key: Hashable & Comparable, Value> : CustomStringConvertible, Map {
+	var keys = [Key]()
+	var values = [Value]()
+	
+	mutating func set (_ key: Key, _ value: Value) {
+		if let index = keys.index(of: key) {
+			values[index] = value
+		} else {
+			keys.append(key)
+			values.append(value)
+		}
+	}
+	
+	func get (_ key: Key) -> Value? {
+		let index = keys.index(of: key)
+		return index == nil ? nil : values[index!]
+	}
+	
+	var count: Int {
+		get {
+			return keys.count
+		}
+	}
+	
+	var description: String {
+		get {
+			var output = "\(type(of: self)) ["
+			for (index, key) in keys.enumerated() {
+				output += String(describing: key)
+				output += ": "
+				output += String(describing: values[index])
+				output += ", "
+			}
+			if output.last == " " {
+				output.removeLast(2)
+			}
+			output += "]"
+			return output
+		}
+	}
+	
+}
---
+++ map.swift
@@ -0,0 +1,33 @@
+//
+//  map.swift
+//  Maps Benchmark
+//
+//  Created by Gideon Rabson on 11/2/18.
+//  Copyright © 2018 Gideon Rabson. All rights reserved.
+//
+
+import Foundation
+
+protocol Map: CustomStringConvertible {
+	associatedtype Key: Hashable, Comparable
+	associatedtype Value
+	
+	mutating func set (_: Key, _: Value)
+	func get (_: Key) -> Value?
+	
+	var count: Int {get}
+	var description: String {get}
+	
+}
+
+extension Map {
+	subscript (_ k: Key) -> Value? {
+		set {
+			set(k, newValue!)
+		}
+		get {
+			return get(k)
+		}
+	}
+	
+}
